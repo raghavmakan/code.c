379db2f238458a35220cfd172a225c5d7078e74f
raghavmakan <101700022+raghavmakan@users.noreply.github.com>
2022-03-21 12:15:06 +0530

    Update resume.c

--- resume.c
+++ resume.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-	printf("\n NAME:-Siddhant Gulati.");
+	printf("\n NAME:-Raghav.");
 	printf("\n ADDRESS:-I-7/2,sec 16 rohini, delhi");
 	printf("\n EDUCATION:- Pursuing Bachelor of Computer Applications");
 	printf("\n MOBILE:- +91 9911XXXXXX \n\n");
